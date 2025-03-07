8be73e030479803ca473b4ad52016cdbacaac172
Ayush_developer <2023csb1108@iitrpr.ac.in>
2025-03-07 12:57:02 +0530

    trigger deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    'output': 'export',
     images: {
         unoptimized: true
+    },
+    experimental: {
+        serverActions: true
     }
 };
 
 export default nextConfig;
-
